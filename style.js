ad3346c9324b9e95ddad581b802b380048384969
Superstar-IT <freewebdeveloper@hotmail.com>
2020-02-27 01:20:37 +0600

    'new'

---
+++ style.js
@@ -0,0 +1,15 @@
+function onSecSave() {
+	alert("Save");
+}
+
+function fnc_showpass() {
+	alert("fnc_showpass");
+}
+
+function forgetPass() {
+	alert("forgetPass");
+}
+
+$('#login .main-body-right .keyboard-row .btn2').click(function(){
+	alert($(this).val());
+});
